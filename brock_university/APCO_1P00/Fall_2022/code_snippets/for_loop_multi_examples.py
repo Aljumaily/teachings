ce5913d10831bbc889700f7ab0864c7528befc55
Maysara Al Jumaily <aljumaily.maysara@gmail.com>
2022-09-05 05:22:49 -0400

    init commit

--- brock_university/APCO_1P00/Fall_2022/code_snippets/for_loop_multi_examples.py
+++ brock_university/APCO_1P00/Fall_2022/code_snippets/for_loop_multi_examples.py
@@ -1,11 +1,11 @@
 for i in range(3, 14, 2):
-	print(i)
+	print(i, end=" ")
 # output: 3 5 7 9 11 13
-
+print(" ")
 for i in range(-10, 2, 1):
-	print(i)
+	print(i, end=" ")
 # output: -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1
-
+print(" ")
 for i in range(10, 3, -1):
-	print(i)
+	print(i, end=" ")
 # output: 10 9 8 7 6 5 4
